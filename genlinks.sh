e7874762ac9e92ca5039e3fdb0ed70f132304d2c
U-CHARLIE\jdunne <jdunne@225-JDUNNE.charlie.kaplaninc.com>
2011-06-15 11:50:37 -0500

    Restructured folders and included git kdiff3 helper scripts.

--- genlinks.sh
+++ genlinks.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 pushd ~
-rm -f .bashrc && ln -s dotfiles/bashrc .bashrc
-rm -f .gitconfig && ln -s dotfiles/gitconfig .gitconfig
-rm -f .gitk && ln -s dotfiles/gitk .gitk
-rm -f .inputrc && ln -s dotfiles/inputrc .inputrc
-rm -f .minttyrc && ln -s dotfiles/minttyrc .minttyrc
-rm -f .nanorc && ln -s dotfiles/nanorc .nanorc
-rm -f .profile && ln -s dotfiles/profile .profile
-rm -f .screenrc && ln -s dotfiles/screenrc .screenrc
-rm -f .viminfo && ln -s dotfiles/viminfo .viminfo
+rm -f .bashrc && ln -s ~/dotfiles/home/bashrc .bashrc
+rm -f .gitconfig && ln -s ~/dotfiles/home/gitconfig .gitconfig
+rm -f .gitk && ln -s ~/dotfiles/home/gitk .gitk
+rm -f .inputrc && ln -s ~/dotfiles/home/inputrc .inputrc
+rm -f .minttyrc && ln -s ~/dotfiles/home/minttyrc .minttyrc
+rm -f .nanorc && ln -s ~/dotfiles/home/nanorc .nanorc
+rm -f .profile && ln -s ~/dotfiles/home/profile .profile
+rm -f .screenrc && ln -s ~/dotfiles/home/screenrc .screenrc
 popd
