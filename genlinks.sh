821eb8473bb05ce03f7328a767e7dd544f31c653
James Dunne <james.jdunne@gmail.com>
2012-01-17 11:26:17 -0600

    Updated genlinks.sh to be safe to run.

--- genlinks.sh
+++ genlinks.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 pushd ~
-rm -f .bashrc && ln -s ~/dotfiles/home/bashrc .bashrc
-rm -f .gitconfig && ln -s ~/dotfiles/home/gitconfig .gitconfig
-rm -f .gitk && ln -s ~/dotfiles/home/gitk .gitk
-rm -f .inputrc && ln -s ~/dotfiles/home/inputrc .inputrc
-rm -f .minttyrc && ln -s ~/dotfiles/home/minttyrc .minttyrc
-rm -f .nanorc && ln -s ~/dotfiles/home/nanorc .nanorc
-rm -f .profile && ln -s ~/dotfiles/home/profile .profile
-rm -f .screenrc && ln -s ~/dotfiles/home/screenrc .screenrc
-# mkdir -p ~/.ssh
-# ln -s ~/dotfiles/home/ssh/* .ssh/
+mkdir -p .backup
+mv -n .bashrc .backup/
+ln -s ~/dotfiles/home/bashrc .bashrc
+mv -n .gitconfig .backup/
+ln -s ~/dotfiles/home/gitconfig .gitconfig
+mv -n .gitk .backup/
+ln -s ~/dotfiles/home/gitk .gitk
+mv -n .inputrc .backup/
+ln -s ~/dotfiles/home/inputrc .inputrc
+mv -n .minttyrc .backup/
+ln -s ~/dotfiles/home/minttyrc .minttyrc
+mv -n .nanorc .backup/
+ln -s ~/dotfiles/home/nanorc .nanorc
+mv -n .profile .backup/
+ln -s ~/dotfiles/home/profile .profile
+mv -n .screenrc .backup/
+ln -s ~/dotfiles/home/screenrc .screenrc
+
+mkdir -p .ssh
+mkdir -p .backup/ssh
+mv -n .ssh/config .ssh/known_hosts .backup/ssh/
+ln -s ~/dotfiles/home/ssh/* .ssh/
 popd
